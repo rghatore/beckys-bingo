5ed27309c7f78538ef2b651c60fcf602a1571e3b
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-01-26 16:41:21 -0700

    adding header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export function Header () {
+  return (
+    <header className="header">
+      <img src="logo192.png" alt="logo-left"/>
+      <h1 className="title">Becky's Bingo</h1>
+      <img src="logo192.png" alt="logo-left"/>
+    </header>
+  )  
+}
+
+// export default Header;
