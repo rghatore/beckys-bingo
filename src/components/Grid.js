bca4b86e5dae0b06eefd771564042fdcd43e06f0
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-01-28 22:35:23 -0700

    couting and reloading working well

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,6 @@
 import './Grid.scss'
-import { shuffle } from '../helpers/selectors';
+import { clearSelected, maxNumber, shuffle } from '../helpers/selectors';
+import { useEffect, useState } from 'react';
 
 export function Grid (props) {
 
@@ -17,14 +18,25 @@ export function Grid (props) {
     bingoItems = items[2];
   }
 
+  useEffect(() => {
+    console.log('Clearing...')
+    clearSelected();
+  },[current])
+
   // add class to cell on click
   const selectCell = (event) => {
     const target = event.target;
-    if (target.className === 'selected') {
-      target.className = '';
+    if ([...target.classList].includes('selected')) {
+      // target.className = '';
+      target.classList.remove('selected');
     } else {
-      target.className = 'selected';
+      // target.className = 'selected';
+      target.classList.add('selected');
     }
+    //  get max number count
+    const count = maxNumber();
+    
+
   }
 
   // currently, FREE cells are random - might change to fixed position
@@ -33,87 +45,87 @@ export function Grid (props) {
       <table>
         <tbody onClick={(event) => selectCell(event)}>
           <tr>
-            <td className={bingoItems[0] ? '' : 'selected'}>
+            <td className={`first sixth eleventh ${bingoItems[0] ? '' : 'selected'}`}>
               {bingoItems[0]  ? bingoItems[0] : 'FREE'}
             </td>
-            <td className={bingoItems[1] ? '' : 'selected'}>
+            <td className={`first seventh ${bingoItems[1] ? '' : 'selected'}`}>
               {bingoItems[1]  ? bingoItems[1] : 'FREE'}
             </td>
-            <td className={bingoItems[2] ? '' : 'selected'}>
+            <td className={`first eighth ${bingoItems[2] ? '' : 'selected'}`}>
               {bingoItems[2]  ? bingoItems[2] : 'FREE'}
             </td>
-            <td className={bingoItems[3] ? '' : 'selected'}>
+            <td className={`first ninth ${bingoItems[3] ? '' : 'selected'}`}>
               {bingoItems[3]  ? bingoItems[3] : 'FREE'}
             </td>
-            <td className={bingoItems[4] ? '' : 'selected'}>
+            <td className={`first tenth twelfth ${bingoItems[4] ? '' : 'selected'}`}>
               {bingoItems[4]  ? bingoItems[4] : 'FREE'}
             </td>
           </tr>
           <tr>
-            <td className={bingoItems[5] ? '' : 'selected'}>
+            <td className={`second sixth ${bingoItems[5] ? '' : 'selected'}`}>
               {bingoItems[5]  ? bingoItems[5] : 'FREE'}
             </td>
-            <td className={bingoItems[6] ? '' : 'selected'}>
+            <td className={`second seventh eleventh ${bingoItems[6] ? '' : 'selected'}`}>
               {bingoItems[6]  ? bingoItems[6] : 'FREE'}
             </td>
-            <td className={bingoItems[7] ? '' : 'selected'}>
+            <td className={`second eighth ${bingoItems[7] ? '' : 'selected'}`}>
               {bingoItems[7]  ? bingoItems[7] : 'FREE'}
             </td>
-            <td className={bingoItems[8] ? '' : 'selected'}>
+            <td className={`second ninth twelfth ${bingoItems[8] ? '' : 'selected'}`}>
               {bingoItems[8]  ? bingoItems[8] : 'FREE'}
             </td>
-            <td className={bingoItems[9] ? '' : 'selected'}>
+            <td className={`second tenth ${bingoItems[9] ? '' : 'selected'}`}>
               {bingoItems[9]  ? bingoItems[9] : 'FREE'}
             </td>
           </tr>
           <tr>
-            <td className={bingoItems[10] ? '' : 'selected'}>
+            <td className={`third sixth ${bingoItems[10] ? '' : 'selected'}`}>
               {bingoItems[10] ? bingoItems[10] : 'FREE'}
             </td>
-            <td className={bingoItems[11] ? '' : 'selected'}>
+            <td className={`third seventh ${bingoItems[11] ? '' : 'selected'}`}>
               {bingoItems[11] ? bingoItems[11] : 'FREE'}
             </td>
-            <td className={bingoItems[12] ? '' : 'selected'}>
+            <td className={`third eighth eleventh twelfth ${bingoItems[12] ? '' : 'selected'}`}>
               {bingoItems[12] ? bingoItems[12] : 'FREE'}
             </td>
-            <td className={bingoItems[13] ? '' : 'selected'}>
+            <td className={`third ninth ${bingoItems[13] ? '' : 'selected'}`}>
               {bingoItems[13] ? bingoItems[13] : 'FREE'}
             </td>
-            <td className={bingoItems[14] ? '' : 'selected'}>
+            <td className={`third tenth ${bingoItems[14] ? '' : 'selected'}`}>
               {bingoItems[14] ? bingoItems[14] : 'FREE'}
             </td>
           </tr>
           <tr>
-            <td className={bingoItems[15] ? '' : 'selected'}>
+            <td className={`fourth sixth ${bingoItems[15] ? '' : 'selected'}`}>
               {bingoItems[15] ? bingoItems[15] : 'FREE'}
             </td>
-            <td className={bingoItems[16] ? '' : 'selected'}>
+            <td className={`fourth seventh twelfth ${bingoItems[16] ? '' : 'selected'}`}>
               {bingoItems[16] ? bingoItems[16] : 'FREE'}
             </td>
-            <td className={bingoItems[17] ? '' : 'selected'}>
+            <td className={`fourth eighth ${bingoItems[17] ? '' : 'selected'}`}>
               {bingoItems[17] ? bingoItems[17] : 'FREE'}
             </td>
-            <td className={bingoItems[18] ? '' : 'selected'}>
+            <td className={`fourth ninth eleventh ${bingoItems[18] ? '' : 'selected'}`}>
               {bingoItems[18] ? bingoItems[18] : 'FREE'}
             </td>
-            <td className={bingoItems[19] ? '' : 'selected'}>
+            <td className={`fourth tenth ${bingoItems[19] ? '' : 'selected'}`}>
               {bingoItems[19] ? bingoItems[19] : 'FREE'}
             </td>
           </tr>
           <tr>
-            <td className={bingoItems[20] ? '' : 'selected'}>
+            <td className={`fifth sixth twelfth ${bingoItems[20] ? '' : 'selected'}`}>
               {bingoItems[20] ? bingoItems[20] : 'FREE'}
             </td>
-            <td className={bingoItems[21] ? '' : 'selected'}>
+            <td className={`fifth seventh ${bingoItems[21] ? '' : 'selected'}`}>
               {bingoItems[21] ? bingoItems[21] : 'FREE'}
             </td>
-            <td className={bingoItems[22] ? '' : 'selected'}>
+            <td className={`fifth eighth ${bingoItems[22] ? '' : 'selected'}`}>
               {bingoItems[22] ? bingoItems[22] : 'FREE'}
             </td>
-            <td className={bingoItems[23] ? '' : 'selected'}>
+            <td className={`fifth ninth ${bingoItems[23] ? '' : 'selected'}`}>
               {bingoItems[23] ? bingoItems[23] : 'FREE'}
             </td>
-            <td className={bingoItems[24] ? '' : 'selected'}>
+            <td className={`fifth tenth eleventh ${bingoItems[24] ? '' : 'selected'}`}>
               {bingoItems[24] ? bingoItems[24] : 'FREE'}
             </td>
           </tr>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,58 @@
-export function shuffle (items) {
+export function shuffle(items) {
   for (let i = items.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [items[i], items[j]] = [items[j], items[i]];
   }
 };
+
+export function clearSelected() {
+  const allTags = document.querySelectorAll('td');
+  // console.log('working ...', allTags);
+  allTags && allTags.forEach(item => {
+    item.classList.remove('selected')
+    // console.log('looping: ', item);
+    if(item.innerHTML === 'FREE') {
+      item.classList.add('selected');
+    }
+  });
+};
+
+const findMax = (search, max) => {
+  // initialize
+  let count = 0;
+  // get bingo line
+  const bingoLine = document.querySelectorAll(`.${search}`);
+  // loop through one bingo line
+  // add each selected one to count
+  bingoLine.forEach(item => {
+    if ([...item.classList].includes('selected')) {
+      count++;
+    }
+  });
+  // replace max if count > max
+  if (count > max) {
+    max = count;
+  };
+  // output
+  return max
+}
+
+export function maxNumber() {
+  let max = 0;
+  // first, second, ..., twelfth
+  max = findMax('first', max);
+  max = findMax('second', max);
+  max = findMax('third', max);
+  max = findMax('fourth', max);
+  max = findMax('fifth', max);
+  max = findMax('sixth', max);
+  max = findMax('seventh', max);
+  max = findMax('eighth', max);
+  max = findMax('ninth', max);
+  max = findMax('tenth', max);
+  max = findMax('eleventh', max);
+  max = findMax('twelfth', max);
+
+  // console.log('max: ', max);
+  return max;
+}
